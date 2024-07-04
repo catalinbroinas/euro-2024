0ad20f829bf78ea27c765bf0e3d0daf45df96b5b
catalinbroinas <catalinbroinas@yahoo.com>
2024-07-04 14:57:09 +0300

    Add `utility` module with `domHandler`  * Implement `clearPageContent` method: clear container

---
+++ src/js/module/utility.js
@@ -0,0 +1,15 @@
+function domHandler() {
+    const clearPageContent = (container) => {
+        if (container.hasChildNodes()) {
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+        }
+    };
+
+    return {
+        clearPageContent
+    };
+}
+
+export { domHandler };

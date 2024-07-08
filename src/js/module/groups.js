dbc843d575f085ff159f833cf17faf54572f4ef7
catalinbroinas <catalinbroinas@yahoo.com>
2024-07-08 16:22:25 +0300

    Update `getTable` and `getResults` methods

--- src/js/module/groups.js
+++ src/js/module/groups.js
@@ -1,11 +1,14 @@
 async function TableDataFetcher() {
     const getTable = async () => {
         try {
-            const response = await fetch('/src/json/groups.json', { mode: "cors" });
+            const response = await fetch('/src/json/groupsTable.json', { mode: "cors" });
             if (!response.ok) {
                 throw new Error('Groups data response was not ok.');
             }
             const groups = await response.json();
+            if (!groups || !Array.isArray(groups)) {
+                throw new Error('Groups table data is not in expected format.');
+            }
             return groups;
         } catch (err) {
             console.error('There has been a problem with your fetch operation:', err);
@@ -18,11 +21,14 @@ async function TableDataFetcher() {
 async function MatchesDataFetcher() {
     const getResults = async () => {
         try {
-            const response = await fetch('/src/json/results.json', { mode: "cors" });
+            const response = await fetch('/src/json/groupsResults.json', { mode: "cors" });
             if (!response.ok) {
                 throw new Error('Groups results data response was not ok.');
             }
             const results = await response.json();
+            if (!results || !Array.isArray(results)) {
+                throw new Error('Groups results data is not in expected format.');
+            }
             return results;
         } catch (err) {
             console.error('There has been a problem with your fetch operation:', err);

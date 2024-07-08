8f5ce5299fdd0887361a7fd0484982eb1ef1c399
catalinbroinas <catalinbroinas@yahoo.com>
2024-07-08 16:04:14 +0300

    Add `getTable` method to fetch and parse data from `groups.json`

--- src/js/module/groups.js
+++ src/js/module/groups.js
@@ -1,5 +1,18 @@
 async function TableDataFetcher() {
+    const getTable = async () => {
+        try {
+            const response = await fetch('/src/json/groups.json', { mode: "cors" });
+            if (!response.ok) {
+                throw new Error('Groups data response was not ok.');
+            }
+            const groups = await response.json();
+            return groups;
+        } catch (err) {
+            console.error('There has been a problem with your fetch operation:', err);
+        }
+    };
 
+    return { getTable };
 }
 
 async function MatchesDataFetcher() {

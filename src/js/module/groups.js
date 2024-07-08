535c056f5d742fe44497acd2fb32e7c2cc9eb274
catalinbroinas <catalinbroinas@yahoo.com>
2024-07-08 17:03:04 +0300

    Refactor data fetching logic in groups.js module

--- src/js/module/groups.js
+++ src/js/module/groups.js
@@ -1,43 +1,31 @@
-async function TableDataFetcher() {
-    const getTable = async () => {
-        try {
-            const response = await fetch('/src/json/groupsTable.json', { mode: "cors" });
-            if (!response.ok) {
-                throw new Error('Groups data response was not ok.');
-            }
-            const groups = await response.json();
-            if (!groups || !Array.isArray(groups)) {
-                throw new Error('Groups table data is not in expected format.');
-            }
-            return groups;
-        } catch (err) {
-            console.error('There has been a problem with your fetch operation:', err);
+async function fetchData(url, errorMessage) {
+    try {
+        const response = await fetch(url, { mode: 'cors' });
+        if (!response.ok) {
+            throw new Error(errorMessage);
         }
-    };
+        const data = await response.json();
+        if (!data || !Array.isArray(data)) {
+            throw new Error(`${errorMessage} - data is not in expected format.`);
+        }
+        return data;
+    } catch (err) {
+        console.error('There has been a problem with your fetch operation:', err);
+    }
+}
 
+async function TableDataFetcher() {
+    const getTable = () => fetchData('/src/json/groupsTable.json', 'Groups table data response was not ok.');
     return { getTable };
 }
 
 async function MatchesDataFetcher() {
-    const getResults = async () => {
-        try {
-            const response = await fetch('/src/json/groupsResults.json', { mode: "cors" });
-            if (!response.ok) {
-                throw new Error('Groups results data response was not ok.');
-            }
-            const results = await response.json();
-            if (!results || !Array.isArray(results)) {
-                throw new Error('Groups results data is not in expected format.');
-            }
-            return results;
-        } catch (err) {
-            console.error('There has been a problem with your fetch operation:', err);
-        }
-    };
-
+    const getResults = () => fetchData('/src/json/groupsResults.json', 'Groups results data response was not ok.');
     return { getResults };
-}
+};
 
 function GroupDomHandler() {
 
 }
+
+export { TableDataFetcher, MatchesDataFetcher };

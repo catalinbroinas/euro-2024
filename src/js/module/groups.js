7b0663b08eb1774995bec04da6d16438c67798c5
catalinbroinas <catalinbroinas@yahoo.com>
2024-07-11 16:46:54 +0300

    Add `createTable` method to `GroupDomHandler` Render tables using `groupsTable.json` data

--- src/js/module/groups.js
+++ src/js/module/groups.js
@@ -1,3 +1,5 @@
+import { UtilityDomHandler } from "./utility.js";
+
 async function fetchData(url, errorMessage) {
     try {
         const response = await fetch(url, { mode: 'cors' });
@@ -25,7 +27,92 @@ async function MatchesDataFetcher() {
 };
 
 function GroupDomHandler() {
+    const utilityDom = UtilityDomHandler();
 
+    const createTable = ({
+        groupTitle,
+        headers,
+        data
+    }) => {
+        // Table structure and title 
+        const tableWrapper = utilityDom.createDOMElement({
+            elementTag: 'div',
+            elementClass: ['table-responsive', 'shadow-2-strong', 'table-group'],
+        });
+        const table = utilityDom.createDOMElement({
+            elementTag: 'table',
+            elementClass: ['table', 'table-hover']
+        });
+        const title = utilityDom.createDOMElement({
+            elementTag: 'h3',
+            elementClass: ['title', 'mb-4'],
+            elementText: groupTitle
+        });
+        const tableHead = utilityDom.createDOMElement({
+            elementTag: 'thead',
+            elementClass: ['table-light']
+        });
+        const tableBody = utilityDom.createDOMElement({
+            elementTag: 'tbody'
+        });
+
+        // Table Header
+        const theadRow = utilityDom.createDOMElement({
+            elementTag: 'tr'
+        });
+
+        headers.forEach(header => {
+            const th = utilityDom.createDOMElement({
+                elementTag: 'th',
+                elementScope: 'col',
+                elementClass: header.class ? header.class : '',
+                elementText: header.text
+            });
+            theadRow.appendChild(th);
+        });
+
+        tableHead.appendChild(theadRow);
+
+        // Table Body
+        data.forEach((row) => {
+            const tr = utilityDom.createDOMElement({
+                elementTag: 'tr'
+            });
+
+            row.forEach(cell => {
+                const td = utilityDom.createDOMElement({
+                    elementTag: 'td',
+                    elementClass: cell.class ? cell.class : ''
+                });
+
+                if (cell.icon) {
+                    const icon = utilityDom.createDOMElement({
+                        elementTag: 'i',
+                        elementClass: cell.icon.class,
+                        elementAtrType: cell.icon.atrType,
+                        elementAtrValue: cell.icon.atrValue
+                    });
+                    td.appendChild(icon);
+                }
+
+                if (cell.text) {
+                    const text = document.createTextNode(cell.text);
+                    td.appendChild(text);
+                }
+
+                tr.appendChild(td);
+            });
+
+            tableBody.appendChild(tr);
+        });
+
+        table.appendChild(tableHead);
+        table.appendChild(tableBody);
+        tableWrapper.appendChild(title);
+        tableWrapper.appendChild(table);
+
+        return tableWrapper;
+    };
 }
 
 export { TableDataFetcher, MatchesDataFetcher };

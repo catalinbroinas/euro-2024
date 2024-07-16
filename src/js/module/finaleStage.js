7d3503a1aa809a84a21c4d0121210ea81dce8d9d
catalinbroinas <catalinbroinas@yahoo.com>
2024-07-16 15:15:38 +0300

    Add `createMatch` method to `FinaleStageDomHandler`  * For dynamic match card generation

--- src/js/module/finaleStage.js
+++ src/js/module/finaleStage.js
@@ -1,3 +1,5 @@
+import { UtilityDomHandler } from "./utility.js";
+
 async function FinaleStageDataFetcher() {
     const getResults = async () => {
         try {
@@ -18,4 +20,149 @@ async function FinaleStageDataFetcher() {
     return { getResults };
 }
 
-export { FinaleStageDataFetcher };
+function FinaleStageDomHandler() {
+    const utilityDom = UtilityDomHandler();
+
+    const createMatch = ({
+        homeFlag,
+        awayFlag,
+        homeTeam,
+        awayTeam,
+        homeScore,
+        awayScore,
+        referee,
+        stadium
+    }) => {
+        // Card structure
+        const card = utilityDom.createDOMElement({
+            elementTag: 'div',
+            elementClass: ['card']
+        });
+        const cardBody = utilityDom.createDOMElement({
+            elementTag: 'div',
+            elementClass: ['card-body', 'py-3', 'px-4']
+        });
+        const cardFooter = utilityDom.createDOMElement({
+            elementTag: 'div',
+            elementClass: ['card-footer', 'border-0', 'px-4', 'pb-3', 'pt-0', 'd-flex', 'justify-content-around', 'g-3']
+        });
+        const listGroup = utilityDom.createDOMElement({
+            elementTag: 'ul',
+            elementClass: ['list-group-item', 'd-flex', 'justify-content-between', 'align-items-center']
+        });
+
+        // Home Team Item
+        const homeItem = utilityDom.createDOMElement({
+            elementTag: 'li',
+            elementClass: ['list-group-item', 'd-flex', 'justify-content-between', 'align-items-center']
+        });
+        const homeTeamDiv = utilityDom.createDOMElement({
+            elementTag: 'div',
+            elementClass: 'd-flex align-items-center'
+        });
+        const homeTeamImg = utilityDom.createDOMElement({
+            elementTag: 'img',
+            elementClass: 'me-4 avatar-team',
+            elementAttributes: {
+                src: homeFlag,
+                alt: `${homeTeam} flag`
+            }
+        });
+        const homeTeamText = utilityDom.createDOMElement({
+            elementTag: 'h5',
+            elementClass: ['text', 'm-0'],
+            elementText: homeTeam
+        });
+        const homeScoreText = utilityDom.createDOMElement({
+            elementTag: 'h5',
+            elementClass: 'title m-0',
+            elementText: homeScore
+        });
+
+        homeTeamDiv.appendChild(homeTeamImg);
+        homeTeamDiv.appendChild(homeTeamText);
+        homeItem.appendChild(homeTeamDiv);
+        homeItem.appendChild(homeScoreText);
+
+        // Away Team Item
+        const awayItem = utilityDom.createDOMElement({
+            elementTag: 'li',
+            elementClass: ['list-group-item', 'd-flex', 'justify-content-between', 'align-items-center']
+        });
+        const awayTeamDiv = utilityDom.createDOMElement({
+            elementTag: 'div',
+            elementClass: 'd-flex align-items-center'
+        });
+        const awayTeamImg = utilityDom.createDOMElement({
+            elementTag: 'img',
+            elementClass: 'me-4 avatar-team',
+            elementAttributes: {
+                src: awayFlag,
+                alt: `${awayTeam} flag`
+            }
+        });
+        const awayTeamText = utilityDom.createDOMElement({
+            elementTag: 'h5',
+            elementClass: 'text m-0',
+            elementText: awayTeam
+        });
+        const awayScoreText = utilityDom.createDOMElement({
+            elementTag: 'h5',
+            elementClass: 'title m-0',
+            elementText: awayScore
+        });
+
+        awayTeamDiv.appendChild(awayTeamImg);
+        awayTeamDiv.appendChild(awayTeamText);
+        awayItem.appendChild(awayTeamDiv);
+        awayItem.appendChild(awayScoreText);
+
+        listGroup.appendChild(homeItem);
+        listGroup.appendChild(awayItem);
+        cardBody.appendChild(listGroup);
+
+        // Card footer
+        const refereeInfo = utilityDom.createDOMElement({
+            elementTag: 'h6',
+            elementClass: 'text small m-0',
+        });
+        const refereeIcon = utilityDom.createDOMElement({
+            elementTag: 'i',
+            elementClass: ['fas', 'fa-diamond', 'me-2'],
+            elementAttributes: {
+                'data-mdb-toggle': 'tooltip',
+                'data-mdb-placement': 'bottom',
+                title: 'Referee'
+            }
+        });
+
+        const stadiumInfo = utilityDom.createDOMElement({
+            elementTag: 'h6',
+            elementClass: 'text small m-0',
+        });
+        const stadiumIcon = utilityDom.createDOMElement({
+            elementTag: 'i',
+            elementClass: ['fas', 'fa-location-dot', 'me-2'],
+            elementAttributes: {
+                'data-mdb-toggle': 'tooltip',
+                'data-mdb-placement': 'bottom',
+                title: 'Stadium'
+            }
+        });
+
+        refereeInfo.appendChild(refereeIcon);
+        refereeInfo.textContent += referee;
+
+        stadiumInfo.appendChild(stadiumIcon);
+        stadiumInfo.textContent += stadium;
+
+        cardFooter.appendChild(refereeInfo);
+        cardFooter.appendChild(stadiumInfo);
+        card.appendChild(cardBody);
+        card.appendChild(cardFooter);
+
+        return card;
+    };
+}
+
+export { FinaleStageDataFetcher, FinaleStageDomHandler };

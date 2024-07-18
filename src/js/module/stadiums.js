82e5159e5fedfdc2c8324f1022a7a3cd39832220
catalinbroinas <catalinbroinas@yahoo.com>
2024-07-18 12:18:38 +0300

    Add `getStadiums` method to `StadiumDataFetcher`  * Fetch and parse data from `stadiums.json`

--- src/js/module/stadiums.js
+++ src/js/module/stadiums.js
@@ -1,5 +1,22 @@
 async function StadiumDataFetcher() {
+    const getStadiums = async () => {
+        try {
+            const response = await fetch('/src/json/stadiums.json', { mode: 'cors' });
+            if (!response.ok) {
+                throw new Error('Stadiums data response was not ok.');
+            }
+            const data = await response.json();
+            if (!data || !Array.isArray(data)) {
+                throw new Error('Stadiums data is not in expected format.');
+            }
+            return data;
+        } catch (err) {
+            console.error('There has been a problem with your fetch operation:', err);
+            return [];
+        }
+    };
 
+    return { getStadiums };
 }
 
 function StadiumDomHandler() {
